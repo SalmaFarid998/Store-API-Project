20b8705e853f1f964eba90f4ecfa0d529c5fe427
salmafareed998 <salmafareed998@gmail.com>
2024-10-19 23:28:42 +0300

    Part 06 - Repository Layer - Unit Of Work

---
+++ Store.Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Store.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<TEntity,Tkey> Repository<TEntity,Tkey>() where TEntity : BaseEntity<Tkey>;
+        Task<int> CompletedAsync();
+    }
+}
