68db9acc0f181f44e4fa6dc15c067f6945c73639
salmafareed998 <salmafareed998@gmail.com>
2024-10-22 23:40:25 +0300

    Part 06 - Exception Middleware

---
+++ Store.Web/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Store.Service.HandleResponse;
+using System.Net;
+using System.Text.Json;
+
+namespace Store.Web.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment environment;
+
+        public ExceptionMiddleware(RequestDelegate Next, ILogger<ExceptionMiddleware> logger, IHostEnvironment environment)
+        {
+            next = Next;
+            this.logger = logger;
+            this.environment = environment;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                {
+                    logger.LogError(ex, ex.Message);
+                    context.Response.ContentType= "application/json";
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                    var ResponseEnv = environment.IsDevelopment() ? new CustomException(500,ex.Message,ex.StackTrace.ToString()):new CustomException((int)HttpStatusCode.InternalServerError);
+
+                    var Options = new JsonSerializerOptions()
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    };
+                    var jsonResponse = JsonSerializer.Serialize(ResponseEnv, Options); 
+                    await context.Response.WriteAsync(jsonResponse);
+                }
+            }
+        }
+    }
+}
